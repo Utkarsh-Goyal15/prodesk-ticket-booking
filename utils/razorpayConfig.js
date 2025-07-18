caa2b09dfdcfed49cb6e2e4295b2f12116c61f32
Utkarsh goyal <utkarshgoyal1503@gmail.com>
2025-07-18 08:43:18 +0530

    Update razorpayConfig.js

--- utils/razorpayConfig.js
+++ utils/razorpayConfig.js
@@ -1,7 +1,7 @@
-// import Razorpay from 'razorpay';
+import Razorpay from 'razorpay';
 
 
-// export const instance = new Razorpay({
-//     key_id: process.env.razorpay_ApiKey,
-//     key_secret: process.env.razorpayKeySecret,
-// });
+export const instance = new Razorpay({
+    key_id: process.env.razorpay_ApiKey,
+    key_secret: process.env.razorpayKeySecret,
+});
